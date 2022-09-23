cc10a3065a933e0d788956b0cb55744d5b522886
lev sviridov <levgeneral@yandex.ru>
2022-09-24 01:52:29 +0300

    REFACTOR ADDED requirements

--- batch_schedule_lessons.py
+++ batch_schedule_lessons.py
@@ -2,12 +2,23 @@ import csv
 import requests
 
 
-def get_subject_id_by_name_and_type(subject_dict: dict[str, str]) -> (int | None):
+def get_week_type_id_by_name(week_type_name: str) -> (str | None):
+    if week_type_name == 'верхняя':
+        return '1'
+    elif week_type_name == 'нижняя':
+        return '0'
+    else:
+        print('No corresponding week type id for ' + week_type_name)
+        print()
+
+def get_subject_id_by_name_and_type(subject_dict: dict[str, str]) -> (str | None):
     query = {
         'name': subject_dict['name'],
         'subject_type': subject_dict['subject_type'],
     }
+
     response = requests.get(ROOT_URL + 'subject' + '/', params=query)
+
     try:
         return response.json()[0]['id']
     except:
@@ -15,20 +26,22 @@ def get_subject_id_by_name_and_type(subject_dict: dict[str, str]) -> (int | None
         print('Subject URL: ' + response.url)
         print()
 
-def get_subject_type_id_by_naming(subject_type):
-    if subject_type == 'Лекция':
-        return 0
-    elif subject_type == 'Практика':
-        return 1
+def get_subject_type_id_by_name(subject_type_name: str) -> (str | None):
+    if subject_type_name == 'Лекция':
+        return '0'
+    elif subject_type_name == 'Практика':
+        return '1'
     else:
-        print('No corresponding subject type for ' + subject_type)
+        print('No corresponding subject type for ' + subject_type_name)
         print()
 
-def get_group_id_by_name(group_name):
+def get_group_id_by_name(group_name: str) -> (str | None):
     query = {
         'name': group_name,
     }
+
     response = requests.get(ROOT_URL + 'student_group' + '/', params=query)
+
     try:
         return response.json()[0]['id']
     except:
@@ -36,13 +49,15 @@ def get_group_id_by_name(group_name):
         print(response.url)
         print()
 
-def get_teacher_id_by_full_name(teacher_object):
+def get_teacher_id_by_full_name(teacher_object: dict[str, str]) -> (str | None):
     query = {
         'first_name': teacher_object['first_name'],
         'middle_name': teacher_object['middle_name'],
         'last_name': teacher_object['last_name'],
     }
+
     response = requests.get(ROOT_URL + 'teacher' + '/', params=query)
+    
     try:
         return response.json()[0]['id']
     except:
@@ -50,47 +65,54 @@ def get_teacher_id_by_full_name(teacher_object):
         print(response.url)
         print()
 
-def create_lesson(lesson_obj):
-    response = requests.post(ROOT_URL + 'schedule' + '/', json=lesson_obj)
-    # print('Creating: ', lesson_obj)
-    # print('Lesson creation status:', response.text)
-    # print()
+def create_lesson(lesson_obj:dict[str, str]) -> None:
+    if all(schedule_lesson_obj.values()):
+        response = requests.post(ROOT_URL + 'schedule' + '/', json=lesson_obj)
+        # print('Creating: ', lesson_obj)
+        # print('Lesson creation status:', response.text)
+        # print()
+    else:
+        print('Some schedule_lesson_obj values are empty:')
+        print(schedule_lesson_obj)
+        print('Discarding POST to create')
+        print('=============================================')
+
 
 
 ROOT_URL = 'http://localhost:8000/api/'
 FILE_PATH = 'setmag_lessons.csv'
 
-WEEK_TYPE_COL = 0
+WEEK_TYPE_NAME_COL = 0
 
-DAY_NUMBER_COL = 1
+DAY_ID_COL = 1
 
-LESSON_NUMBER_COL = 2
-LESSON_NAME_COL = 3
-LESSON_TYPE_COL = 4
+LESSON_ID_COL = 2
+SUBJECT_NAME_COL = 3
+SUBJECT_TYPE_NAME_COL = 4
 
 GROUP_NAME_COL = 5
 
-TEACHER_SECOND_NAME_COL = 6
+TEACHER_LAST_NAME_COL = 6
 TEACHER_FIRST_NAME_COL = 7
-TEACHER_THIRD_NAME_COL = 8
+TEACHER_MIDDLE_NAME_COL = 8
 
-with open(FILE_PATH, 'r') as csv_file:
-    reader = csv.reader(csv_file)
+with open(FILE_PATH, 'r', encoding='utf-8') as csv_file:
+    data = csv.reader(csv_file)
 
-    for csv_row in reader:
-        week_type = csv_row[WEEK_TYPE_COL]
+    for csv_row in data:
+        week_type_name = csv_row[WEEK_TYPE_NAME_COL]
 
-        day_number = csv_row[DAY_NUMBER_COL]
+        day_id = csv_row[DAY_ID_COL]
 
-        lesson_number = csv_row[LESSON_NUMBER_COL]
-        subject_name = csv_row[LESSON_NAME_COL]
-        subject_type_naming = csv_row[LESSON_TYPE_COL]
+        lesson_id = csv_row[LESSON_ID_COL]
+        subject_name = csv_row[SUBJECT_NAME_COL]
+        subject_type_name = csv_row[SUBJECT_TYPE_NAME_COL]
 
         group_name = csv_row[GROUP_NAME_COL]
 
-        teacher_last_name = csv_row[TEACHER_SECOND_NAME_COL]
+        teacher_last_name = csv_row[TEACHER_LAST_NAME_COL]
         teacher_first_name = csv_row[TEACHER_FIRST_NAME_COL]
-        teacher_middle_name = csv_row[TEACHER_THIRD_NAME_COL]
+        teacher_middle_name = csv_row[TEACHER_MIDDLE_NAME_COL]
 
         schedule_lesson_obj = {
             'week': '',
@@ -101,29 +123,18 @@ with open(FILE_PATH, 'r') as csv_file:
             'teacher': '',
         }
 
-        if week_type == 'верхняя':
-            schedule_lesson_obj['week'] = '1'
-        else:
-            schedule_lesson_obj['week'] = '0'
-
-        schedule_lesson_obj['day'] = day_number
-        schedule_lesson_obj['lesson'] = lesson_number
-        schedule_lesson_obj['group'] = get_group_id_by_name(group_name)
-    
+        schedule_lesson_obj['week'] = get_week_type_id_by_name(week_type_name)
+        schedule_lesson_obj['day'] = day_id
+        schedule_lesson_obj['lesson'] = lesson_id
         schedule_lesson_obj['subject'] = get_subject_id_by_name_and_type({
             'name': subject_name,
-            'subject_type': get_subject_type_id_by_naming(subject_type_naming),
+            'subject_type': get_subject_type_id_by_name(subject_type_name),
         })
-    
+        schedule_lesson_obj['group'] = get_group_id_by_name(group_name)
         schedule_lesson_obj['teacher'] = get_teacher_id_by_full_name({
             'first_name': teacher_first_name,
             'last_name': teacher_last_name,
             'middle_name': teacher_middle_name,
         })
-    
-        if not all(schedule_lesson_obj.values()):
-            print('Some schedule_lesson_obj values are empty:')
-            print(schedule_lesson_obj)
-            print('Discarding POST to create')
-        else:
-            create_lesson(schedule_lesson_obj)
+
+        create_lesson(schedule_lesson_obj)
